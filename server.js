caf2a70bc4adc1a34f53e2e666ecb34340518584
jazminnova <jasminnova@gmail.com>
2024-03-03 15:35:12 -0500

    made some edits

--- server.js
+++ server.js
@@ -7,16 +7,24 @@ const PORT = process.env.PORT || 3000;
 //Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
 
 //Static Middleware
 app.use(express.static("public"));
 
-//Routes
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+//
+app.use("/api", api);
+
+//Get route for notes page
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "./public/notes.html"))
+);
+
+// get route for homepage
+res.get("index", (req, res) =>
+  res.sendFile(path.join(__dirname, "./public/index.html"))
+);
 
 //Listener
 app.listen(PORT, function () {
-  console.log("App listening on PORT: " + PORT);
+  console.log("App listening on http://localhost:${PORT}");
 });
